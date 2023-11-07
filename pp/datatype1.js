fffd77f880e3601d9bd69c6c417f1d8d10e019d9
MAHESH RODE <130589113+maheshrode01@users.noreply.github.com>
2023-11-07 18:28:02 +0000

    change file

--- pp/datatype1.js
+++ pp/datatype1.js
@@ -26,3 +26,29 @@ console.log(str3,+num3)
 
 
 
+console.log("*********************************************************")
+ 
+console.log("2"===2);
+
+
+
+const heros=["rushi","lakhan"]// array
+let myObj={
+    name:"rushi",
+    age: 21,
+
+
+}// all curly in use to object
+
+const myFunction =function(){
+    console.log("hello")
+
+}// all function declare
+
+//******************************
+ 
+Stack and heap 
+stack => duplicate value in primitive
+heap=> same value assign in heap  non primitive 
+
+
